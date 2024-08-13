417ffaff1a4ee39462c896bc66722154bb03c1b1
Filip Sjöstrand <f.w.sjostrand@gmail.com>
2024-08-13 16:32:14 +0200

    new model implementation and parameters

--- models/modules/lstm/model.py
+++ models/modules/lstm/model.py
@@ -29,12 +29,12 @@ class LSTMAutoencoder:
         model.add(keras.layers.Masking(mask_value=-1))
 
         units = hp.Int("units", min_value=32, max_value=512, step=32)
-        model.add(keras.layers.LSTM(units,return_sequences=True))
-        model.add(keras.layers.LSTM(units//2, return_sequences=False))
+        model.add(keras.layers.LSTM(units,return_sequences=True, dropout=0.5))
+        model.add(keras.layers.LSTM(units//2, return_sequences=False, dropout=0.5))
 
         model.add(keras.layers.RepeatVector(self.input_shape[0]))
 
-        model.add(keras.layers.LSTM(units//2, return_sequences=True))
+        model.add(keras.layers.LSTM(units//2, return_sequences=True, dropout=0.5))
         model.add(keras.layers.LSTM(units, return_sequences=True))
 
         model.add(keras.layers.TimeDistributed(keras.layers.Dense(self.input_shape[1])))
@@ -108,6 +108,7 @@ class LSTMAutoencoder:
             validation_data=val_dataset,
             epochs=500,
             shuffle=False,
+            callbacks=[stop_early],
         )
         logging.model("Getting best epoch")
         val_loss_per_epoch = history.history["val_loss"]
